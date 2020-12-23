e3b20c4254e540571ad151610ff7f21cb8aa2545
d90375 <d90375@gmail.com>
2020-12-23 17:47:59 +0300

    add: cfg

---
+++ config/index.js
@@ -0,0 +1,25 @@
+const { config } = require("dotenv");
+
+const { parsed } = config();
+
+const {
+    PORT,
+    MODE,
+    DATA_BASE_NAME,
+    DATA_BASE_PASSWORD,
+    DATA_BASE_USERNAME,
+    MONGO_DB_CONNECTION_URL = `mongodb+srv://${DATA_BASE_USERNAME}:${DATA_BASE_PASSWORD}@cluster-mova-data-base.bnb39.mongodb.net/${DATA_BASE_NAME}?retryWrites=true&w=majority`,
+    IN_PROD = MODE !== "prod",
+    BASE_URL,
+    URL = `${BASE_URL}${PORT}`
+} = parsed;
+
+module.exports = {
+    PORT,
+    MODE,
+    DATA_BASE_NAME,
+    MONGO_DB_CONNECTION_URL,
+    IN_PROD,
+    BASE_URL,
+    URL
+};
