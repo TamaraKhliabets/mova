6c961001f0325dea1ab4d35421d7fe6901706d1b
Anna Kulay <annakulay10@gmail.com>
2020-12-28 21:03:48 +0300

    feat(back16): add getAllWords

--- src/resources/word/word.controller.js
+++ src/resources/word/word.controller.js
@@ -2,6 +2,9 @@ const wordModel = require('./word.model');
 
 const createWord = async (data) => wordModel.createWord(data);
 
+const getAllWords = async () => wordModel.getAllWords();
+
 module.exports = {
-  createWord
+  createWord,
+  getAllWords
 };
