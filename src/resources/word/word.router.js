75ab416674ebff867a2872b348bed426404fa598
Anna Kulay <annakulay10@gmail.com>
2020-12-28 20:43:33 +0300

    feat(back13): add word

---
+++ src/resources/word/word.router.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const wordController = require('./word.controller');
+const { catchErrors } = require('../../middleware/catchErrors');
+
+// api - для всего?
+router.route('/word').post(
+  catchErrors(async (req, res) => {
+    const word = await wordController.createWord(req.body);
+    return res.status(200).json({ word: word });
+  })
+);
+
+module.exports = router;
