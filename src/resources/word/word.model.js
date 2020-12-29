dfee3b1c266f21d8a1741de528e6b16ed83b4e51
Anna Kulay <annakulay10@gmail.com>
2020-12-29 11:51:30 +0300

    feat(back10): add updateWord

--- src/resources/word/word.model.js
+++ src/resources/word/word.model.js
@@ -6,8 +6,12 @@ const getAllWords = async () => await word.find({});
 
 const getWordById = async (id) => await word.findById(id);
 
+const updateWord = async (id, data) =>
+  await word.updateOne({ _id: id }, { ...data, updated_at: Date.now() });
+
 module.exports = {
   createWord,
   getAllWords,
-  getWordById
+  getWordById,
+  updateWord
 };
