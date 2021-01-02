edca4d4067d9661b20f94d7b4a1dccf9d8d2f967
Anna Kulay <annakulay10@gmail.com>
2021-01-02 20:01:15 +0300

    fix: fix schema

--- src/resources/word/word.router.js
+++ src/resources/word/word.router.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 
 const wordController = require('./word.controller');
+const wordSchema = require('./word.schema');
+
 const { catchErrors } = require('../../middleware/catchErrors');
 
-// api - для всего?
 router.route('/word').post(
   catchErrors(async (req, res) => {
     const word = await wordController.createWord(req.body);
-    return res.status(200).json({ word });
+    return res.status(200).json(wordSchema.toResponse(word));
   })
 );
 
